7f7f73302d10b2a3b50b524c32ecdcbb91a16959
RASHWIN <ashwinikunju27@gmail.com>
2025-06-21 18:18:09 +0530

    adding arrays

---
+++ src/Arrays/sortingTechniques/Insertsort.java
@@ -0,0 +1,30 @@
+package Arrays.sortingTechniques;
+
+public class Insertsort {
+
+    
+    public static void main(String[] args) {
+        int[] arr = {8,7,6,5,4};
+        System.out.println("hello");
+        insertionSort(arr);
+        System.out.println("sorted");
+        for(int x: arr){
+            System.out.println(x + " ");
+        }
+
+    }
+
+    public static void insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 1; i<n ; i++){
+            int j = i-1;
+            int key = arr[i];
+            while(j>-1 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+    }
+}
