76fd26f51224d7c2cc583020f246098a54f0fe42
RASHWIN <ashwinikunju27@gmail.com>
2025-06-27 13:26:10 +0530

    adding linkedlist

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,54 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        ll list = new ll();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//
+//        list.display();
+//
+//        list.insertLast(99);
+//        list.display();
+//        list.insertAtPostion(5,4);
+//        list.display();
+
+//        list.deleteFirst();
+//        list.display();
+//
+//        list.deleteLast();
+//        list.display();
+//        list.deleteAtPosition(4);
+//        list.display();
+
+//        DLL dll = new DLL();
+//        dll.insertFirst(3);
+//        dll.insertFirst(4);
+//        dll.insertFirst(5);
+//        dll.insertFirst(6);
+//        dll.display();
+//
+//        dll.insertLast(9);
+//        dll.display();
+//
+//        dll.insertAfter(5, 65);
+//        dll.display();
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+        list.display();
+
+        list.delete(19);
+        list.display();
+
+
+
+
+
+
+    }
+}
