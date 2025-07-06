e259ef21da657d4ebc4bfad3a5ce47a38d4c7438
RASHWIN <ashwinikunju27@gmail.com>
2025-07-06 11:29:07 +0530

    adding sliding window

--- src/TwoPointers/BuySellStock.java
+++ src/TwoPointers/BuySellStock.java
@@ -7,18 +7,17 @@ public class BuySellStock {
         System.out.println(maxProfit);
     }
     public static int findMaxProfit(int[] arr){
-        int r = 0;
+
         int l = 0;
         int max = Integer.MIN_VALUE;
         int profit = 0;
-        while(r< arr.length){
+        for(int r = 0 ;r< arr.length; r++){
             if(arr[l] < arr[r]){
                 profit = arr[r] - arr[l];
                 max = Math.max(profit, max);
             } else{
                 l =r;
             }
-            r++;
         }
         return (max == Integer.MIN_VALUE)?0: max;
     }
