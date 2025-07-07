46766c1bb02a1fe808e96801415cc6af4009abd3
RASHWIN <ashwinikunju27@gmail.com>
2025-07-07 11:19:07 +0530

    adding twopointers

---
+++ src/TwoPointers/LeftRotationUsingPointers.java
@@ -0,0 +1,29 @@
+package TwoPointers;
+
+public class LeftRotationUsingPointers {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int k = 4;
+        leftRotation(arr, k);
+        for(int x : arr){
+            System.out.println(x);
+        }
+    }
+    public static void leftRotation(int[] arr, int k){
+        int n = arr.length;
+        k = k%n;
+        reverse(arr,0, n-1);
+        reverse(arr, 0 , k-1);
+        reverse(arr, k, n-1);
+    }
+
+    public static void reverse(int[] arr , int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
---
+++ src/TwoPointers/MaxWaterContainer.java
@@ -0,0 +1,38 @@
+package TwoPointers;
+
+public class MaxWaterContainer {
+    public static void main(String[] args) {
+        int[] heights = {1,8,6,2,5,4,8,3,7};
+        int max = findMaxAreaUsingPointers(heights);
+        System.out.println(max);
+    }
+    public static int findMaxArea(int[] heights){
+        int res = 0;
+        for(int i =0 ; i<heights.length ; i++){
+            for(int j = i+1; j<heights.length ; j++){
+                res = Math.max(res, (j-i)* Math.min(heights[i],heights[j]));
+            }
+        }
+
+        return res;
+    }
+
+    public static int findMaxAreaUsingPointers(int[] heights){
+        int l = 0;
+        int r = heights.length -1;
+        int res = Integer.MIN_VALUE;
+        while(l<r){
+            int area = Math.min(heights[l], heights[r])*(r-l);
+            res = Math.max(area,res);
+            if(heights[l] < heights[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+
+        return res;
+
+    }
+}
+
