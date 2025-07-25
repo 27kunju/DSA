0653848b4dcc6a0f9fb47759bb89b0903730df51
ASHWINI RANGASWAMY <ashwini.r@mercedes-benz.com>
2025-07-25 22:47:13 +0530

    adding sliding window

--- src/SlidingWindow/LongestSubString.java
+++ src/SlidingWindow/LongestSubString.java
@@ -1,12 +1,15 @@
 package SlidingWindow;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubString {
     public static void main(String[] args) {
-        String s = "abcdefghacheadhie";
-        int maxSubStringSize = findLongestSubString(s);
+        String s = "abccdefchcacheadhie";
+        int k = 3;
+        int maxSubStringSize = findLongestSubString2(s, k);
         System.out.println(maxSubStringSize);
     }
     public static int findLongestSubString(String s){
@@ -23,5 +26,35 @@ public class LongestSubString {
 
         }
         return res;
+    }
+
+    public static int findLongestSubString2(String s, int k){
+        Map<Character, Integer> charMap = new HashMap<>();
+        int i = 0, j = 0;
+        int max = 0;
+
+        while (j < s.length()) {
+            char endChar = s.charAt(j);
+            charMap.put(endChar, charMap.getOrDefault(endChar, 0) + 1);
+
+            if (charMap.size() < k) {
+                j++;
+            } else if (charMap.size() == k) {
+                max = Math.max(max, j - i + 1);
+                j++;
+            } else {
+                while (charMap.size() > k) {
+                    char startChar = s.charAt(i);
+                    charMap.put(startChar, charMap.get(startChar) - 1);
+                    if (charMap.get(startChar) == 0) {
+                        charMap.remove(startChar);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+
+        return max;
     }
 }
